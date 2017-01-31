478668d6a24cbf5bcfe838caf7ab8c0cf722adfe
Kerry Turner <kerry@unity3d.com>
2017-01-31 15:50:34 +0000

    Updated demo project

--- session-3/demo_project_end_of_session_3/Assets/Scripts/Player.cs
+++ session-3/demo_project_end_of_session_3/Assets/Scripts/Player.cs
@@ -2,31 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
-
+public class Player : MonoBehaviour 
+{
 	int score;
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
 	void OnTriggerEnter(Collider other)
 	{
 		if(other.tag == "Collectable")
 		{
-			Debug.Log("Collided with a Collectable!");
 			Destroy(other.gameObject);
 			score += 1;
 			Debug.Log("Score: " + score);
 		}
 
-		if(other.tag == "Door" && score > 2)
+		if(other.tag == "Door" && score > 1)
 		{
 			Destroy(other.gameObject);
 		}
